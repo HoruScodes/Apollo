8c015a36fac7bc11ed4f5fbf069a8d50adc32605
Horuscodes <pipaliya1997@icloud.com>
2021-04-07 21:42:50 -0400

    add login and register

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -17,15 +18,16 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
-    console.log("serve started on localhost:4000");
+    console.log("server started on localhost:4000");
   });
 };
 
